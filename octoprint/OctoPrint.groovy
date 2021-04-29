de5381b42a0ad04b0895c1766d34245a557e5b4c
michaelbarone <mbarone000@gmail.com>
2021-04-29 14:59:22 -0700

    round completion value, reduce the number of events to 1 per percentage change. fixed unschedule call which was cancelling the logsOff scheduled job added optional api call to get data from the addon DisplayLayerProgress

--- octoprint/OctoPrint.groovy
+++ octoprint/OctoPrint.groovy
@@ -13,7 +13,7 @@ metadata {
         attribute "estimatedPrintTime", "string"
         attribute "name", "string"
         attribute "user", "string"
-		attribute "lastPrinterCheck", "String"
+		attribute "lastPrinterCheck", "string"
         
 		
 		// attributes for temperature
@@ -31,8 +31,35 @@ metadata {
 		attribute "tool2-actual", "number"
 		attribute "tool2-offset", "number"
 		attribute "tool2-target", "number"
-        
-        
+
+
+		// attributes for DisplayLayerProgress addon (if installed and enabled in hubitat device)
+		attribute "DLP_fanSpeed", "string"
+		attribute "DLP_feedrate", "number"
+		attribute "DLP_feedrateG0", "number"
+		attribute "DLP_feedrateG1", "number"
+		attribute "DLP_layer_averageLayerDuration", "string"
+		attribute "DLP_layer_averageLayerDurationInSeconds", "number"
+		attribute "DLP_layer_current", "number"
+		attribute "DLP_layer_lastLayerDuration", "string"
+		attribute "DLP_layer_lastLayerDurationInSeconds", "number"
+		attribute "DLP_layer_total", "number"
+		attribute "DLP_print_changeFilamentCount", "number"
+		attribute "DLP_print_changeFilamentTimeLeft", "string"
+		attribute "DLP_print_changeFilamentTimeLeftInSeconds", "number"
+		attribute "DLP_print_estimatedChangedFilamentTime", "string"
+		attribute "DLP_print_estimatedEndTime", "string"
+		attribute "DLP_print_m73progress", "number"
+		attribute "DLP_print_printerState", "string"
+		attribute "DLP_print_progress", "number"
+		attribute "DLP_print_timeLeft", "string"
+		attribute "DLP_print_timeLeftInSeconds", "number"
+		attribute "DLP_height_current", "number"
+		attribute "DLP_height_currentFormatted", "number"
+		attribute "DLP_height_total", "number"
+		attribute "DLP_height_totalFormatted", "number"
+
+
         command "CheckPrinter", null
         command "Print", null
         command "Restart", null
@@ -48,6 +75,7 @@ metadata {
             input "ip_addr", "string", title:"ip address", description: "", required: true, displayDuringSetup: true
             input "url_port", "string", title:"tcp port", description: "", required: true, displayDuringSetup: true, defaultValue: "80"
             input "api_key", "string", title:"API Key", description: "", required: true, displayDuringSetup: true, defaultValue: ""
+            input "enableDisplayLayerProgress", "bool", title:"Get Layer Progress", description: "Gets Layer Progress.  **Requires the octoprint addon: DisplayLayerProgress**", required: true, displayDuringSetup: true, defaultValue: false
             
             input "delayCheckIdle", "number", title:"Number of seconds between checking printer while idle", description: "", required: true, displayDuringSetup: true, defaultValue: "600"
             input "delayCheckPrinting", "number", title:"Number of seconds between checking printer while printing", description: "After a print, this short delay will be used to refresh the printer data until the Primary Extruder (tool0) is less than 50C", required: true, displayDuringSetup: true, defaultValue: "60"
@@ -64,17 +92,21 @@ void parse(String toparse){
 
 void initialize(){
 	state.isPrinting = false
-	unschedule()
+	unschedule(CheckPrinter)
     if (autoUpdate) runIn(1, CheckPrinter)
+}
+
+void uninstalled(){
+	unschedule()
 }
 
 def updated(){
 	if (logEnable) {
 		log.warn "debug logging enabled..."
 		runIn(1800,logsOff)
-	}	
+	}
 	state.isPrinting = false
-	unschedule()
+	unschedule(CheckPrinter)
     if (autoUpdate) runIn(5, CheckPrinter)
 }
 
@@ -154,7 +186,8 @@ def GetPrinter() {
 				if (state.isPrinting && response.data.progress.completion != null)
 					{
 						//state.completion = response.data.progress.completion
-						sendEvent(name: "completion", value: response.data.progress.completion)
+						def completionValue = Math.round(response.data.progress.completion)
+						sendEvent(name: "completion", value: completionValue)
 					} else {
 						sendEvent(name: "completion", value: 0 )
 					}
@@ -196,6 +229,7 @@ def GetPrinter() {
 						sendEvent(name: "user", value: "none" )
 					}
 				
+				if (logEnable) log.debug "GetPrinterJobReturn"
 				if (logEnable) log.info response.data
 				toReturn = response.data.toString()
 
@@ -203,6 +237,9 @@ def GetPrinter() {
 				// check printer temperatures after successful return of printer job details
 				if(state.printerConnected){
 					GetPrinterTemp()
+					if(enableDisplayLayerProgress){
+						GetDisplayLayerProgress()
+					}
 				} else {
 					ResetTemperatures()
 				}
@@ -244,7 +281,6 @@ def PrinterNotResponding(){
 	sendEvent(name: "presence", value: "not present")
 	state.printerConnected = false
 	sendEvent(name: "state", value: "Disconnected")
-	//sendEvent(name: "stateMessage", value: "none")
 	state.state = "Disconnected"
 	state.isPrinting = false
 	sendEvent(name: "completion", value: 0 )
@@ -255,6 +291,8 @@ def PrinterNotResponding(){
 	sendEvent(name: "user", value: "none" )
 	// reset temperature readings to 0 when disconnected
 	ResetTemperatures()
+	// reset DLP addon readings to 0 when disconnected
+	ResetDisplayLayerProgress()
 }
 
 def ResetTemperatures(){
@@ -275,6 +313,36 @@ def ResetTemperatures(){
 		sendEvent(name: "tool2-offset", value: 0 )
 		sendEvent(name: "tool2-target", value: 0 )
 	}		
+}
+
+def ResetDisplayLayerProgress(){
+	// only reset if enabled AND if there is a previous value for DLP_fanSpeed
+	if(enableDisplayLayerProgress && device.currentValue("DLP_fanSpeed") != null){
+		sendEvent(name: "DLP_fanSpeed", value: 0 )
+		sendEvent(name: "DLP_feedrate", value: 0 )
+		sendEvent(name: "DLP_feedrateG0", value: 0 )
+		sendEvent(name: "DLP_feedrateG1", value: 0 )
+		sendEvent(name: "DLP_layer_averageLayerDuration", value: 0 )
+		sendEvent(name: "DLP_layer_averageLayerDurationInSeconds", value: 0 )
+		sendEvent(name: "DLP_layer_current", value: 0 )
+		sendEvent(name: "DLP_layer_lastLayerDuration", value: 0 )
+		sendEvent(name: "DLP_layer_lastLayerDurationInSeconds", value: 0 )
+		sendEvent(name: "DLP_layer_total", value: 0 )
+		sendEvent(name: "DLP_print_changeFilamentCount", value: 0 )
+		sendEvent(name: "DLP_print_changeFilamentTimeLeft", value: "-" )
+		sendEvent(name: "DLP_print_changeFilamentTimeLeftInSeconds", value: 0 )
+		sendEvent(name: "DLP_print_estimatedChangedFilamentTime", value: "-" )
+		sendEvent(name: "DLP_print_estimatedEndTime", value: 0 )
+		sendEvent(name: "DLP_print_m73progress", value: null )
+		sendEvent(name: "DLP_print_printerState", value: null )
+		sendEvent(name: "DLP_print_progress", value: 0 )
+		sendEvent(name: "DLP_print_timeLeft", value: 0 )
+		sendEvent(name: "DLP_print_timeLeftInSeconds", value: 0 )
+		sendEvent(name: "DLP_height_current", value: 0 )
+		sendEvent(name: "DLP_height_currentFormatted", value: 0 )
+		sendEvent(name: "DLP_height_total", value: 0 )
+		sendEvent(name: "DLP_height_totalFormatted", value: 0 )
+	}
 }
 
 def GetPrinterTemp(){
@@ -299,30 +367,105 @@ def GetPrinterTemp(){
 
 def GetPrinterTempReturn(response, data) {
 	if (logEnable) log.debug "GetPrinterTempReturn"
-	//log.debug response.getJson().temperature
-	def R = response.getJson()
-	def value = ""
-	def event
-	
-	// just processing temperature values
-	if(R.temperature != null){
-		R.temperature.each{ t ->
-			//log.debug t
-			if(t != null){
-				//log.debug t.getValue()
-				t.getValue().each { i ->
-					//log.debug "${t.getKey()}-${i}"
-					value = "${t.getKey()}-${i}"
-					
-					event = value.split("=")
-					
-					//log.debug event[0]
-					//log.debug event[1]
-					
-					sendEvent(name: event[0], value: event[1] )
+	if(response?.status == 200){
+		if (logEnable) log.info response.data
+		def R = response.getJson()
+		def value = ""
+		def event
+		
+		// just processing temperature values
+		if(R.temperature != null){
+			R.temperature.each{ t ->
+				if(t != null){
+					t.getValue().each { i ->
+						value = "${t.getKey()}-${i}"
+						event = value.split("=")
+						sendEvent(name: event[0], value: event[1] )
+					}
 				}
 			}
 		}
+	} else {
+		log.warn "Did not get proper response for GetPrinterTemp"
+		log.warn "${response?.status}"
+	}
+}
+
+def GetDisplayLayerProgress(){
+	if(!enableDisplayLayerProgress){
+		log.warn "GetDisplayLayerProgress called, but enableDisplayLayerProgress is disabled"
+		return
+	} else {
+		if (logEnable) log.debug "GetDisplayLayerProgress"
+	}
+	
+    def wxURI2 = "http://${ip_addr}:${url_port}/plugin/DisplayLayerProgress/values"
+    def requestParams2 =
+	[
+		uri:  wxURI2,
+        headers: [ 
+                   "User-Agent": "Wget/1.20.1",
+                   Accept: "*/*",
+                   "Accept-Encoding": "identity",
+                   Host: "${ip_addr}",
+                   Connection: "Keep-Alive",
+                   "X-Api-Key": "${api_key}",
+                 ],
+	]
+    asynchttpGet('GetDisplayLayerProgressReturn', requestParams2)
+}
+
+def GetDisplayLayerProgressReturn(response, data) {
+	if (logEnable) log.debug "GetDisplayLayerProgressReturn"
+	if(response?.status == 200){
+		if (logEnable) log.info response.data
+		def R = response.getJson()
+		def value = ""
+		def event = ""
+
+		// process top level data
+		if(R.fanSpeed != null){
+			sendEvent(name: "DLP_fanSpeed", value: R.fanSpeed)
+		}
+		if(R.feedrate != null){
+			sendEvent(name: "DLP_feedrate", value: R.feedrate)
+		}
+		if(R.feedrateG0 != null){
+			sendEvent(name: "DLP_feedrateG0", value: R.feedrateG0)
+		}
+		if(R.feedrateG1 != null){
+			sendEvent(name: "DLP_feedrateG1", value: R.feedrateG1)
+		}
+
+		// process layer data
+		if(R.layer != null){
+			R.layer.each{ t ->
+				if(t != null){
+					sendEvent(name: "DLP_layer_" + t.getKey(), value: t.getValue())
+				}
+			}
+		}
+
+		// process print data
+		if(R.print != null){
+			R.print.each{ t ->
+				if(t != null){
+					sendEvent(name: "DLP_print_" + t.getKey(), value: t.getValue())
+				}
+			}
+		}
+
+		// process height data
+		if(R.height != null){
+			R.height.each{ t ->
+				if(t != null){
+					sendEvent(name: "DLP_height_" + t.getKey(), value: t.getValue())
+				}
+			}
+		}
+	} else {
+		log.warn "Did not get proper response for GetDisplayLayerProgress"
+		log.warn "${response?.status}"
 	}
 }
 
